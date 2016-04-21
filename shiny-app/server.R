6c2fc9225be0ac6e8dc85cbc8f03d86dd28bb7cc
Mark <cofiem+git@gmail.com>
2016-04-21 15:57:08 +1000

    added shiny app

---
+++ shiny-app/server.R
@@ -0,0 +1,65 @@
+
+# This is the server logic for a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+r_colors <- rgb(t(col2rgb(colors()) / 255))
+
+names(r_colors) <- colors()
+
+shinyServer(function(input, output, session) {
+
+  # Reactive expression for the data subsetted to what the user selected
+  filteredData <- reactive({
+    site.xy[site.xy$cv >= input$range[1] & site.xy$cv <= input$range[2],]
+  })
+  
+  # This reactive expression represents the palette function,
+  # which changes as the user makes selections in UI.
+  colorpal <- reactive({
+    colorNumeric(input$colors, site.xy$cv)
+  })
+  
+  output$map <- renderLeaflet({
+    # Use leaflet() here, and only include aspects of the map that
+    # won't need to change dynamically (at least, not unless the
+    # entire map is being torn down and recreated).
+    leaflet(site.xy) %>% addTiles() %>%
+      fitBounds(~min(Longitude), ~min(Latitude), ~max(Longitude), ~max(Latitude))
+  })
+  
+  # Incremental changes to the map (in this case, replacing the
+  # circles when a new color is chosen) should be performed in
+  # an observer. Each independent set of things that can change
+  # should be managed in its own observer.
+  observe({
+    pal <- colorpal()
+    
+    leafletProxy("map", data = filteredData()) %>%
+      clearShapes() %>%
+      #addTiles() %>%
+      #addMarkers(~Longitude, ~Latitude)
+      addCircles(radius = ~cv, weight = 1, color = "#777777",
+                 fillColor = "#777777", fillOpacity = 0.7, popup = ~paste(cv)
+      )
+  })
+  
+  # Use a separate observer to recreate the legend as needed.
+  observe({
+    proxy <- leafletProxy("map", data = site.xy)
+    
+    # Remove any existing legend, and only if the legend is
+    # enabled, create a new one.
+    proxy %>% clearControls()
+    if (input$legend) {
+      pal <- colorpal()
+      proxy %>% addLegend(position = "bottomright",
+                          pal = pal, values = ~cv
+      )
+    }
+  })
+
+})
---
+++ shiny-app/ui.R
@@ -0,0 +1,24 @@
+
+# This is the user-interface definition of a Shiny web application.
+# You can find out more about building applications with Shiny here:
+#
+# http://shiny.rstudio.com
+#
+
+library(shiny)
+library(leaflet)
+library(RColorBrewer)
+
+shinyUI(bootstrapPage(
+  tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
+  leafletOutput("map", width = "100%", height = "100%"),
+  absolutePanel(top = 10, right = 10,
+                sliderInput("range", "Magnitudes", min(site.xy$cv), max(site.xy$cv),
+                            value = range(site.xy$cv), step = 0.1
+                ),
+                selectInput("colors", "Color Scheme",
+                            rownames(subset(brewer.pal.info, category %in% c("seq", "div")))
+                ),
+                checkboxInput("legend", "Show legend", TRUE)
+  )
+))
